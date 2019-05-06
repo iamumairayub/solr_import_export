c9ea8f84df679ddb6e2dcfe3b7fcf2e96adadf13
mani619cash <mani619cash@gmail.com>
2019-05-06 13:42:41 +0500

    100% working script

---
+++ solr_import_export.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import json
+import requests
+
+start = 0
+inc = 10000
+
+while 1:
+    q = 'https://www.solr.com/solr/from_solr/select?q=*:*&wt=json&start=%s&rows=%s'%(start, inc)
+    print(q)
+    test = requests.get(q, auth=('userName', 'passWord'))
+    test = json.loads(test.text)
+    
+    if len(test['response']['docs']):
+        for a in test['response']['docs']:
+            del a['_version_']
+
+        response = requests.post('https://www.solr.com/solr/to_solr/update?commit=true', 
+                                 headers={'Content-Type':'application/json'},
+                                 auth=('userName', 'inaem3Fahghi7gepooqu'),
+                                 data=json.dumps(test['response']['docs']))
+        print(response.text)
+        
+        start = start + inc
+    else:
+        print('all done')
+        break
